a396020aaa237db5f62c8b927c64b17d75f62e87
chloeosgood <46794566+chloeosgood@users.noreply.github.com>
2019-05-20 17:00:54 -0500

    my changes to add year to cars and cars2

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,10 +2,17 @@
 then compose them right back into CSV files again and save with
 changed names */
 
+let gulp = require('gulp')
 import { src, dest } from 'gulp'
 import * as rename from 'gulp-rename'
 import { tapCsv } from 'gulp-etl-tap-csv'
 import { targetCsv } from 'gulp-etl-target-csv'
+import { handlelines } from 'gulp-etl-handlelines'
+
+const handleLine = (lineObj: any): object => {
+    lineObj.record["year"] = 2019
+    return lineObj
+}
 
 export function processCsv() {
     return src(['data/*.csv', '!data/*-parsed.csv'])
@@ -13,14 +20,15 @@ export function processCsv() {
         console.log('Starting processing on ' + file.basename)
     })  
     .pipe(tapCsv({ columns:true }))
+    .pipe(handlelines({propsToAdd: {["year"]:2019}},{transformCallback: handleLine}))
     .pipe(rename({ extname: ".ndjson" })) // rename to *.ndjson
     .on('data', function (file:any) {
         console.log('Done parsing ' + file.basename)
     })  
-    .pipe(targetCsv({header:true}))
+    //.pipe(targetCsv({header:true}))
     .pipe(rename({suffix:"-parsed", extname: ".csv" })) // rename to *.ndjson
     .on('data', function (file:any) {
         console.log('Done processing on ' + file.basename)
     })  
-    .pipe(dest('data/'));
+    .pipe(gulp.dest('data/'));
 }
